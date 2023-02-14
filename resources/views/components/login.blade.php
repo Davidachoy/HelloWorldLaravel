e5f19517c6aeae2ee301bf4d661dd329ee7e7f57
Daniel <daniel.brenes@efika.solutions>
2023-02-13 21:32:43 -0600

    names

--- resources/views/components/login.blade.php
+++ resources/views/components/login.blade.php
@@ -61,11 +61,20 @@ body {
   margin-bottom: 15px;
 }
 .secondColor{
+  display: flex;
+  padding-right: 10px;
   background-color: #F29D35;
   color: #F29D35;
   width: '100%';
   flex: 1;
   height: -webkit-fill-available;
+  justify-content: flex-end !important;
+  align-items: flex-end !important;
+}
+
+#names{
+  color: black;
+  font: Icon
 }
 
 .form-signin{
@@ -162,7 +171,15 @@ body {
         </form>
         
       </main>
-      <h1 class="secondColor"></h1>
+      <h1 class="secondColor">
+        <pre id='names'>
+          David Achoy Yakimova
+          Daniel Brenes Reyes
+          Andy Cruz Elizondo
+          Jose Sequeira Chacón
+          Jefry Cuendiz Cuendiz 
+        </pre>
+      </h1>
     
     </body>
     </body>
